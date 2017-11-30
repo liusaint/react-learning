dbb36cd6406dfc5052048c68ec2a2d2905e8d57f
Runner <841766635@qq.com>
2017-12-01 00:00:58 +0800

    一点修改

--- react-book/make-react-redux/src/header.js
+++ react-book/make-react-redux/src/header.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-class Header extends Component {
-    static contextTypes = {
-        store: PropTypes.object
-    }
 
-    constructor() {
-        super()
-        this.state = {
-            themeColor: ''
+const connect = (WrappedCom)=>{
+    class ConnectClass extends Component{
+        render(){
+            return (
+                <div>
+                <WrappedCom />
+                </div>
+                )
+            }
         }
     }
-    componentWillMount(){
-    	this._updateThemeColor()
-    	var {store} = this.context;
-    	store.subscribe(() => this._updateThemeColor())
-    }
-	_updateThemeColor(){
-		var {store} = this.context;
-		var state = store.getState();
-		this.setState({themeColor:state.themeColor})
-	}
-    render() {
+
+
+
+
+
+
+
+    class Header extends Component {
+        static contextTypes = {
+            store: PropTypes.object
+        }
+
+        constructor() {
+            super()
+            this.state = {
+                themeColor: ''
+            }
+        }
+        componentWillMount(){
+           this._updateThemeColor()
+           var {store} = this.context;
+           store.subscribe(() => this._updateThemeColor())
+       }
+       _updateThemeColor(){
+          var {store} = this.context;
+          var state = store.getState();
+          this.setState({themeColor:state.themeColor})
+      }
+      render() {
         return (
-            <div style={{color:this.state.themeColor}}>
-			React.js小书
-			</div>
+        <div style={{color:this.state.themeColor}}>
+        React.js小书
+        </div>
         )
     }
 }
