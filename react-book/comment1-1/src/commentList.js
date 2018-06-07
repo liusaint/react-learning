4a4dd49f22f3c1563e65f80bfee602048ddd13e2
Runner <841766635@qq.com>
2018-06-07 16:46:38 +0800

    prop验证

--- react-book/comment1-1/src/commentList.js
+++ react-book/comment1-1/src/commentList.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 
 import Comment from './comment.js';
-
+import PropTypes from 'prop-types'
 
 class CommentList extends Component {
-  static defaultProps = {
-    comments: []
-  }
-  render() {
-  	var comments = this.props.comments;
-    return (
-    	<ul>
-    	{comments.map((comment,i)=><Comment key={i} comment={comment}></Comment>)}
-    	</ul>
-    );
-  }
+	static defaultProps = {
+		comments: []
+	}
+	static propTypes = {
+		comments: PropTypes.array
+	}
+  	render() {
+	  	var comments = this.props.comments;
+	    return (
+	    	<ul>
+	    	{comments.map((comment,i)=><Comment key={i} comment={comment}></Comment>)}
+	    	</ul>
+	    );
+  	}
 }
 
 export default CommentList;
