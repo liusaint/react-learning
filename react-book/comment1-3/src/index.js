79793f553c7c42398e08fc89855c32d71696b258
Runner <841766635@qq.com>
2018-06-18 22:56:13 +0800

    删除

--- react-book/comment1-3/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CommentApp from './commentApp';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<CommentApp />, document.getElementById('root'));
-registerServiceWorker();
